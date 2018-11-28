d33fbda0dfaecf9481cc6ac3040d2f1369c4d954
KhangTran <khang.tran@axonactive.com>
2018-11-28 17:00:32 +0700

    Init

--- PreferencesKotlin/src/main/java/com/github/tntkhang/prefkotlin/PrefKot.kt
+++ PreferencesKotlin/src/main/java/com/github/tntkhang/prefkotlin/PrefKot.kt
@@ -1,16 +1,24 @@
 package com.github.tntkhang.prefkotlin
 
+import android.content.Context
 import android.content.SharedPreferences
 
 class PrefKot {
-    var instance: PrefKot
-    val pref: SharedPreferences
+    private lateinit var instance: PrefKot
+    var pref: SharedPreferences
 
+    constructor(context: Context) {
+        pref = context.applicationContext.getSharedPreferences(context.getString(R.string.app_name), Context.MODE_PRIVATE)
+    }
 
-    class AA {
+    constructor(context: Context, prefName: String) {
+        pref = context.applicationContext.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+    }
+
+    class Instance {
         companion object {
-            fun getInstance(): SharedPreferences {
-
+            fun setVal(value: String) {
+                pref.
             }
         }
     }
